164f5b2da37c191538c83737d8f7f1ff72fe5fd8
vovak <vova.kurgan@yandex.ru>
2020-03-31 15:36:50 +0500

    Переписан add()

--- add.py
+++ add.py
@@ -2,47 +2,40 @@ import os
 import os.path
 import zipfile
 import hashlib
+from shutil import copyfile
 
 from atomic import hash_obj
 
 CVS_DIR_NAME = '.aw'
 CVS_REPOS_INDEX = 'index'
 CVS_DIR_OBJ_NAME = 'objects'
+TMP_FILE = 'tmp'
 
 
 def add(path):
     """Добавляет файл/директорию в индексируемые,
-    сохраняет текущие изменения в папке object"""
+    сохраняет текущие изменения в папке object
+    параметр path - путь до добавляемого файла"""
+
     path = os.path.normpath(path)
 
     if os.path.isdir(path):
         return add_tree(path)
-    index_addr = os.path.join(CVS_DIR_NAME, CVS_REPOS_INDEX)
+
     hashed_path = hash_obj(path)
     save_obj(path, hashed_path)
-    index_content = ''
-    with open(index_addr, 'r') as index:
-        index_content = index.readlines()
-    check_change = False
+    change_index(path, hashed_path)
 
-    if index_content:
-        for i in index_content:
-            file_info = i.split(' ')
-            # index_path, index_hash = index_line.split()
-            if file_info[0] == path:
-                file_info[1] = hashed_path.strip()
-                check_change = True
-                index_content[index_content.index(i)] =\
-                    ' '.join(file_info)+'\n'
-                break
-            
-    if check_change:
-        with open(index_addr, 'w') as index:
-            for i in index_content:
-                index.write(i)
-    else:
-        with open(index_addr, 'a') as index:
-            index.write(f'{path} {hashed_path}\n')
+
+def add_tree(path, commit=False):
+    """Разбираем содержимое директории рекурсивно"""
+    objects = os.listdir(path)
+    for obj in objects:
+        fullpath = os.path.join(path, obj)
+        if os.path.isdir(fullpath):
+            add_tree(fullpath)
+        else:
+            add(fullpath)
 
 
 def save_obj(path, file_hash):
@@ -62,12 +55,28 @@ def save_obj(path, file_hash):
         zp.write(path, arcname=splt_path[1])
 
 
-def add_tree(path, commit=False):
-    """Разбираем содержимое директории рекурсивно"""
-    objects = os.listdir(path)
-    for obj in objects:
-        fullpath = os.path.join(path, obj)
-        if os.path.isdir(fullpath):
-            add_tree(fullpath)
-        else:
-            add(fullpath)
+def change_index(path, hashed_path):
+    """Обновление индекса"""
+
+    index_addr = os.path.join(CVS_DIR_NAME, CVS_REPOS_INDEX)
+    tmp_file = os.path.join(CVS_DIR_NAME, TMP_FILE)
+    is_changed = False
+    with open(index_addr, 'r') as index:
+        with open(tmp_file, 'w') as tmp:
+            while True:
+                index_line = index.readline()
+                if not index_line:
+                    break
+                index_path, index_hash = index_line.split()
+                if index_path == path:
+                    if index_hash != hashed_path:
+                        index_hash = hashed_path
+                    is_changed = True
+                index_line = ' '.join([index_path, index_hash])
+                tmp.write(index_line + ' \n')
+    if is_changed:
+        copyfile(tmp_file, index_addr)
+    else:
+        with open(index_addr, 'a') as index:
+            index.write(f'{path} {hashed_path}\n')
+    os.remove(tmp_file)
